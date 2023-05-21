028b2cb1b1cd8718aa8e7e01de52d611fb45425a
Marina Portugal <marinapsvreis@gmail.com>
2023-05-21 17:32:41 -0300

    :recycle: Removendo import desnecessário

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { Copyright } from '@/components/Copyright'
-import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { Profile } from '@/components/Profile'
 import { SignIn } from '@/components/SignIn'
